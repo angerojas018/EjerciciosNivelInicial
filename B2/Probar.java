ca630cf2cf800406ba459e3923046b3a14697239
angerojas018 <159502863+angerojas018@users.noreply.github.com>
2024-04-24 12:16:55 -0500

    Probar

---
+++ B2/Probar.java
@@ -0,0 +1,14 @@
+package B2;
+
+
+public class Probar {
+    
+    public static void main(String[] args) {
+        
+        Moto m1 = new Moto();
+        
+        m1.setVelocidad(-5);
+        System.out.println("La velocidad de la moto es:" + m1.velocidad + "kms");
+  
+    }
+}
