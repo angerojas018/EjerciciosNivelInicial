7c4668733e14e94a67581cd9bd16e5b15ff7029a
angerojas018 <159502863+angerojas018@users.noreply.github.com>
2024-04-24 12:22:33 -0500

    Prueba

---
+++ E5/Prueba.java
@@ -0,0 +1,19 @@
+package E5;
+
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+        Semaforo semaforo1 = new Semaforo();
+        
+        // Prueba del método cambio()
+        semaforo1.cambio();
+        semaforo1.cambio();
+        semaforo1.cambio();
+        semaforo1.cambio();
+        semaforo1.cambio();
+        semaforo1.cambio();
+        semaforo1.cambio();
+        
+    }
+}
