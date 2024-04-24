6a13fd946a134bc78cbdcb7e8dc87a9fbe7d6c63
angerojas018 <159502863+angerojas018@users.noreply.github.com>
2024-04-24 12:15:41 -0500

    Prueba

---
+++ A1/Prueba.java
@@ -0,0 +1,44 @@
+package A1;
+
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+    Triangulo t1 = new Triangulo();
+    Triangulo t2 = new Triangulo();
+    
+    
+    t1.setAltura(4);
+    t1.setBase(5);
+    
+        System.out.println("\nTriangulo 1:");
+        System.out.println("Base: " + t1.getBase());
+        System.out.println("Altura: " + t1.getAltura());
+        System.out.println("Área: " + t1.Area());
+        
+   t2.setAltura(2);
+   t2.setBase(4);
+        System.out.println("\nTriangulo 2:");
+        System.out.println("Base: " + t2.getBase());
+        System.out.println("Altura: " + t2.getAltura());
+        System.out.println("Área: " + t2.Area());
+        
+        //Cambiar valores
+        
+    t1.setBase(8);
+    t1.setAltura(10);
+        
+        System.out.println("\nTriangulo 1 despues de cambiar el area y la base:");
+        System.out.println("Base: " + t1.getBase());
+        System.out.println("Altura: " + t1.getAltura());
+        System.out.println("Área: " + t1.Area());
+        
+    t2.setBase(4);
+    t2.setAltura(8);
+   
+        System.out.println("\nTriangulo 2 despues de cambiar el are y la base:");
+        System.out.println("Base: " + t2.getBase());
+        System.out.println("Altura: " + t2.getAltura());
+        System.out.println("Área: " + t2.Area());
+    }
+}
